27dfe2180017ccbe1fdf957cfcddaa39a50d7c55
Dean Morin <morin.dean@gmail.com>
2022-01-28 07:58:36 -0800

    wip

--- tap_postgres/sync_strategies/logical_replication.py
+++ tap_postgres/sync_strategies/logical_replication.py
@@ -679,10 +679,11 @@ def sync_tables(conn_info, logical_streams, state, end_lsn, state_file):
                         with open(state_file, mode="r", encoding="utf-8") as fh:
                             state_comitted = json.load(fh)
                     except Exception:
-                        LOGGER.debug('Unable to open and parse %s', state_file)
+                        LOGGER.info('Unable to open and parse %s', state_file)
                     finally:
+                        LOGGER.info('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
                         lsn_comitted = min(
-                            [get_bookmark(state_comitted, s['tap_stream_id'], 'lsn') for s in logical_streams])
+                            [get_bookmark_wrapper(state_comitted, s['tap_stream_id']) for s in logical_streams])
                         if (lsn_currently_processing > lsn_comitted) and (lsn_comitted > lsn_to_flush):
                             lsn_to_flush = lsn_comitted
                             LOGGER.info('Confirming write up to %s, flush to %s',
@@ -713,3 +714,9 @@ def sync_tables(conn_info, logical_streams, state, end_lsn, state_file):
         singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
     return state
+
+
+def get_bookmark_wrapper(state_comitted, tap_stream_id):
+    LOGGER.info(f'tap_stream_id: {tap_stream_id}')
+    LOGGER.info(f'state_comitted: {state_comitted}')
+    return get_bookmark(state_comitted, tap_stream_id, 'lsn')
